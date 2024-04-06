27af9d47087d45c0f6bea9500d7c80420a532462
andrewk0l <andyoged92@gmail.com>
2024-04-06 22:49:18 +0100

    styling and perfecting the navbar

--- my-app/src/Components/Navbar/Navbar.jsx
+++ my-app/src/Components/Navbar/Navbar.jsx
@@ -6,29 +6,32 @@ import search_icon from '../../assets/search.png'
 import upload_icon from '../../assets/upload.png'
 import more_icon from '../../assets/more.png'
 import notification_icon from '../../assets/notification.png'
-import profile_icon from '../../assets/andy.jpg'
+import profile_icon from '../../assets/andy.png'
 
 
 const Navbar = () => {
   return (
-    <nav classname = 'flex-div'>
+    <nav classname = "flex-div">
       <div classname = 'nav-left flex-div'>
-        <img className='menu_icon' src = {menu_icon} alt = "" />
+        <img className='menu-icon' src = {menu_icon} alt = "" />
         <img className='logo' src= {logo} alt=''/>
       </div>
-
-      <div className='nav-middle flex-div'>
+ 
+      <div className= "nav-middle flex-div">
+        <div className='search-box flex-div'>
         <input type='text' placeholder='Search'/>
         <img src={search_icon} alt=''/>
+        </div>
+        
       </div>
 
       <div className='nav-right flex-div'>
         <img src={upload_icon} alt=''/>
         <img src={more_icon} alt=''/>
         <img src={notification_icon} alt=''/>
-        <img src={profile_icon} className='user_icon'  width={150} height={150} alt=''/>
+        <img src={profile_icon} className='user-icon' alt=''/>
       </div> 
-      
+
     </nav>
   )
 }
