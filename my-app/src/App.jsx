13be20980e8e73c5be3f9e7edb0468e787af43b5
andrewk0l <andyoged92@gmail.com>
2024-04-07 15:10:30 +0100

    creating the sidebar component

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,10 +1,19 @@
 import React from "react";
 import Navbar from "./Components/Navbar/Navbar.jsx"
+import { Route, Routes } from "react-router-dom";
+import Home from "./Pages/Home/Home.jsx";
+import Video from "./Pages/Videos/Video.jsx";
+
+
+
 const App = () => {
   return (
     <div>
       <Navbar/>
-  
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/video/:categoryId/:videoId" element={<Video/>}/>
+      </Routes>
 
     </div>
   )
